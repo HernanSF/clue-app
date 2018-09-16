bc1f61f55862886c5295025020c6be76f8b25d19
hernanSF <hernan23.3.88@gmail.com>
2018-09-16 03:32:37 -0300

    Background (almost?)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import './imagenes/background.jpg';
+import background from './imagenes/background.jpg';
 import Sheet from './components/sheet';
+import BackgroundImage from 'react-background-image-loader';
 
 const chars = ['Col. Mustard', 'Miss Scarlet', 'Mrs. White', 'Mrs. Peacock', 'Mr. Green', 'Professor Plum']
 const weaps = ['Candlestick', 'Revolver', 'Wrench', 'Lead Pipe', 'Knife', 'Rope']
 const scenes = ['Library', 'Conservatory', 'Hall', 'Ballroom', 'Dining Room', 'Lounge', 'Billiard Room', 'Kitchen', 'Study']
-const iconSet = [{ icon:'', color: 'white'}, { icon: 'N', color: 'red'}, { icon: 'S', color: 'green'}, { icon: 'Q1', color: 'yellow'}, { icon: 'Q2', color: 'yellow'}, { icon: 'Q3', color: 'yellow'}, { icon: 'Q4', color: 'yellow'} ]
+const iconSet = [{ icon:'', color: ''}, { icon: 'N', color: 'red'}, { icon: 'S', color: 'green'}, { icon: 'Q1', color: 'yellow'}, { icon: 'Q2', color: 'yellow'}, { icon: 'Q3', color: 'yellow'}, { icon: 'Q4', color: 'yellow'} ]
 
 class App extends Component {
   render() {
     return (
      <div id="app">
-      <li><img src="./imagenes/background.jpg" width="200" height="200"/></li>
-      <Sheet columns="5" characters={chars} weapons={weaps} murderScenes={scenes} iconSet={iconSet}></Sheet>
+    <BackgroundImage className="back-image" src={background}>
+        <div className="back-image-title">
+          Nada
+        </div>
+        <Sheet columns="5" characters={chars} weapons={weaps} murderScenes={scenes} iconSet={iconSet}></Sheet>
+    </BackgroundImage>
      </div>
     );
   }
